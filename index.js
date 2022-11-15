7e6ecae557c0276ce544a590b1d6dfced6b05277
Yun Chen <Yunchenfly@gmail.com>
2022-11-15 18:06:04 -0500

    commit tree a2db26c8678ec0ce9e6ae1cf6103ceb28a665a4c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(function(drivers){
+        return drivers.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function(drivers){
+        return drivers.slice(0, string.length) === string
+    })
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(function(drivers){
+        return drivers.name === string
+    })
+}
